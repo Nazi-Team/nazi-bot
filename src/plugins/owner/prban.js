590562ba2852f4a4b752e747898865b00ca14411
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-14 11:56:56 -0500

    Refactorizar el comando 'prban' para usar la nueva estructura de ejecución y mejorar la gestión de errores al obtener información de IPs.

--- src/plugins/owner/prban.js
+++ src/plugins/owner/prban.js
@@ -5,8 +5,8 @@ export default {
     description: "Banear una IP fraudulenta",
     params: ["ip"],
     comand: ["prban"],
-    async execute(client, message, args) {
-        const ip = args[0];
+    exec: async (m, { sock }) => {
+        const ip = m.args[0];
         const loadingMessages = [
             "「  ✦ 𝐂𝐇𝐄𝐂𝐊𝐄𝐑 𝐃𝐄 𝐁𝐀𝐍 ✦  」\n⟨░░░░░░░░░░░░░░░░░░░░⟩ \n…. Cargando\n↻ 0%",
             "「  ✦ 𝐂𝐇𝐄𝐂𝐊𝐄𝐑 𝐃𝐄 𝐁𝐀𝐍 ✦  」\n⟨██░░░░░░░░░░░░░░░░░░⟩ \n…. Cargando\n⟳ 10%",
@@ -24,31 +24,38 @@ export default {
         let sentMessage;
         for (const msg of loadingMessages) {
             if (!sentMessage) {
-                sentMessage = await client.sendMessage(message.from, { text: msg });
+                sentMessage = await sock.sendMessage(m.from, { text: msg });
             } else {
-                await client.sendMessage(message.from, { text: msg, edit: sentMessage.key });
+                await sock.sendMessage(m.from, { text: msg, edit: sentMessage.key });
             }
             await new Promise(resolve => setTimeout(resolve, 60000)); // Esperar 1 minuto entre mensajes
         }
 
-        const { data } = await axios.get(`https://api11.scamalytics.com/alessandrovillogas/?key=df8d714f6807d3c5512914caed1302f17fb41728efa01d695a9db72734854f34&ip=${ip}`);
+        try {
+            const response = await axios.get(`https://api11.scamalytics.com/alessandrovillogas/?key=df8d714f6807d3c5512914caed1302f17fb41728efa01d695a9db72734854f34&ip=${ip}`);
+            const data = response.data;
 
-        const resultMessage = `
-「  ✦ 𝐂𝐇𝐄𝐂𝐊𝐄𝐑 𝐃𝐄 𝐁𝐀𝐍 ✦  」
-Estado: ${data.status}
-Modo: ${data.mode}
-IP: ${data.ip}
-Puntuación: ${data.score}
-Riesgo: ${data.risk}
-ISP: ${data['ISP Name']}
-Organización: ${data['Organization Name']}
-País: ${data.ip_country_name}
-Ciudad: ${data.ip_city}
-Código Postal: ${data.ip_postcode}
-Geolocalización: ${data.ip_geolocation}
-Tipo de Proxy: ${data.proxy_type}
-ASN: ${data.as_number}`
+            const resultMessage = `
+                「  ✦ 𝐂𝐇𝐄𝐂𝐊𝐄𝐑 𝐃𝐄 𝐁𝐀𝐍 ✦  」
+                Estado: ${data.status}
+                Modo: ${data.mode}
+                IP: ${data.ip}
+                Puntuación: ${data.score}
+                Riesgo: ${data.risk}
+                ISP: ${data['ISP Name']}
+                Organización: ${data['Organization Name']}
+                País: ${data.ip_country_name}
+                Ciudad: ${data.ip_city}
+                Código Postal: ${data.ip_postcode}
+                Geolocalización: ${data.ip_geolocation}
+                Tipo de Proxy: ${data.proxy_type}
+                ASN: ${data.as_number}
+            `;
 
-        await client.sendMessage(message.from, { text: resultMessage, edit: sentMessage.key })
+            await sock.sendMessage(m.from, { text: resultMessage, edit: sentMessage.key });
+        } catch (error) {
+            console.error(error);
+            await sock.sendMessage(m.from, { text: `Error al intentar obtener la información de la IP ${ip}.`, edit: sentMessage.key });
+        }
     }
 }
